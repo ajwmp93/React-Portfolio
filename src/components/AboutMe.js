eb6076557ecba3153925f6dbb1f9218b0cb285e5
Ashley Wright <ashleywright@iMac.local>
2024-10-07 13:11:40 -0400

    added logic to about me page

---
+++ src/components/AboutMe.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <section id='about'>
+            <p>My name is Ashley and I am working toward becoming a full stack developer. I graduated from The Ohio State University with a degree in Korean Language and have been comsistently trying to learn new things. This is my portfolio and all of the projects I have done during my time in this course.</p>
+        </section>
+    );
+};
+
+export default AboutMe;
