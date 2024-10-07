46f574571277e958a80e3902177d0ee2dbd25302
Ashley Wright <ashleywright@iMac.local>
2024-10-07 12:24:34 -0400

    added footer logic

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <a href="https://github.com/ajwmp93">GitHub</a>
+            <a href="https://www.linkedin.com/feed/">LinkedIn</a>
+        </footer>
+    );
+};
+
+export default Footer;
